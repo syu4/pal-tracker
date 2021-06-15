1ce07ed01fbf792f2c97de432e959fcda38e2ed0
sharon <syu4@ford.com>
2021-06-15 16:18:33 +0000

    commiting code

--- src/main/java/io/pivotal/pal/tracker/WelcomeController.java
+++ src/main/java/io/pivotal/pal/tracker/WelcomeController.java
@@ -1,13 +1,24 @@
 package io.pivotal.pal.tracker;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class WelcomeController {
 
+    private final String msg;
+    public WelcomeController(@Value("${welcome.message}") String message
+
+    ) {
+        msg = message;
+
+    }
+
     @GetMapping("/")
     public String sayHello() {
-        return "hello!";
+
+
+        return msg;
     }
 }
